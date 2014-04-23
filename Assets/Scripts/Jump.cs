c78f4d3e3fbb30020c07d286be3e84637e8e4d6f
jdude0822 <jnordlan@purdue.edu>
2014-04-23 13:28:56 -0400

    Changed jumping to be more of what we want
    jump jump jump.....

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class Jump : MonoBehaviour 
 {
 
-	Vector3 jump = new Vector3(0,11f,0);
-	Vector3 fall = new Vector3(0,-50f,0);
+	Vector3 jump = new Vector3(0,13f,0);
+	Vector3 fall = new Vector3(0,-40f,0);
+	Vector3 velocity;
+	Rigidbody player;
 	bool pressed, waitActive;
 	Texture2D wingsUp;
 	Texture2D wingsDown;
 	// Use this for initialization
 	void Start () 
 	{
+		player = GetComponent<Rigidbody>();
 		wingsUp = Resources.Load("Gustavo wings down") as Texture2D;
 		wingsDown = Resources.Load ("Gustavo wings up") as Texture2D;
 		pressed = false;
@@ -21,7 +24,7 @@ public class Jump : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-
+		velocity = player.velocity;
 		if(Input.touchCount > 0)
 		{
 			//Debug.Log(pressed.ToString() + ": " + Input.touchCount);
@@ -34,10 +37,11 @@ public class Jump : MonoBehaviour
 
 			else
 			{
-				this.rigidbody.AddForce(fall);
-
-
-					StartCoroutine(Wait());
+				if(velocity.y > -15)
+				{
+					this.rigidbody.AddForce(fall);
+				}
+				StartCoroutine(Wait());
 			}
 		}
 
@@ -46,7 +50,11 @@ public class Jump : MonoBehaviour
 			//Debug.Log(pressed.ToString() + ": " + Input.touchCount);
 			pressed = false;
 			renderer.material.mainTexture = wingsDown;
-			this.rigidbody.AddForce(fall);
+
+			if(velocity.y > -15)
+			{
+				this.rigidbody.AddForce(fall);
+			}
 		}
 	}
 
@@ -54,7 +62,7 @@ public class Jump : MonoBehaviour
 	{
 		waitActive = true;
 		Debug.Log ("waiting...");
-		yield return new WaitForSeconds(.5f);
+		yield return new WaitForSeconds(.1f);
 		Debug.Log ("Done Waiting!");
 		waitActive = false;
 	}
