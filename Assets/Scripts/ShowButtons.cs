6552b09be1956e2114da4836bc6738b4f7fd4dec
jdude0822 <jnordlan@purdue.edu>
2014-04-29 11:57:21 -0400

    Minor GUI tweaks
    Added backgrounds to buttons, changed some text to make it less wordy,
    added some (temporary) textures to make stuff look slightly better

--- Assets/Scripts/ShowButtons.cs
+++ Assets/Scripts/ShowButtons.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShowButtons : MonoBehaviour {
+public class ShowButtons : MonoBehaviour 
+{
 
 	Touch test;
 	GameObject player = null;
@@ -11,12 +12,13 @@ public class ShowButtons : MonoBehaviour {
 	bool checkedScore = false;
 	string scoreList;
 	string username = "Enter Name for High-Scores";
-
 	bool pressed = true;
 	bool waiting = false;
+	public GUIStyle buttonStyle, textBoxStyle; 
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		nameSpace = new Rect(10, 20, Screen.width - 20, 100);
 		restart = new Rect(10, Screen.height - 160,(Screen.width / 2) - 15, 120);
 		scores = new Rect((Screen.width / 2) + 5, Screen.height - 160, (Screen.width / 2) - 15, 120);
@@ -26,57 +28,68 @@ public class ShowButtons : MonoBehaviour {
 	}
 
 	void Update(){
-		if(Input.touchCount > 0){
-			if(!pressed && !waiting){
+		if(Input.touchCount > 0)
+		{
+			if(!pressed && !waiting)
+			{
 				pressed = true;
 				test = Input.GetTouch(0);
 
 				//Debug.Log("pressed pixel: " + test.position.x + ", " + test.position.y);
-				if(test.position.y >= 40 && test.position.y <= 160){
-					if(!checkedScore && test.position.x >= 10 && test.position.x <= (Screen.width/2)-5){
+				if(test.position.y >= 40 && test.position.y <= 160)
+				{
+					if(!checkedScore && test.position.x >= 10 && test.position.x <= (Screen.width/2)-5)
+					{
 						checkedScore = false;
 						player.GetComponent<StopScripts>().setGameStatus(2);
-						if(string.Compare(username, "Enter Name for High-Scores") != 0){
+						if(string.Compare(username, "Enter Name for High-Scores") != 0)
+						{
 
 							this.gameObject.GetComponent<Scores>().addUser(username, player.GetComponent<StopScripts>().getScore());
 						}
 						this.gameObject.GetComponent<ShowButtons>().enabled = false;
 
 					}
-					else if(checkedScore && test.position.x >= 10 && test.position.x <= Screen.width - 10){
+					else if(checkedScore && test.position.x >= 10 && test.position.x <= Screen.width - 10)
+					{
 						checkedScore = false;
 						player.GetComponent<StopScripts>().setGameStatus(2);
 						restart = new Rect(10, Screen.height - 160,(Screen.width / 2) - 15, 120);
 						player.GetComponent<StopScripts>().hideScores();
 						this.gameObject.GetComponent<ShowButtons>().enabled = false;
 					}
-					else if(test.position.x >= (Screen.width / 2) + 10 && test.position.x <= Screen.width - 5){
+					else if(test.position.x >= (Screen.width / 2) + 10 && test.position.x <= Screen.width - 5)
+					{
 						StartCoroutine(WaitToSwitch());
 					}
 				}
-				else{
+				else
+				{
 					StartCoroutine(pressWait());
 				}
 			}
 		}
-		else{
+		else
+		{
 			pressed = false;
 
 		}
 	}
 
-	void OnGUI(){
-		GUI.Button(restart, "Recompile!\n(Restart)");
-
-		if(!checkedScore){
-			GUI.Button(scores, "High-Scores");
-			username = GUI.TextField(nameSpace , username, 30);
+	void OnGUI()
+	{
+		GUI.Button(restart, "Recompile!", buttonStyle);
+		if(!checkedScore)
+		{
+			GUI.Button(scores, "High-Scores", buttonStyle);
+			username = GUI.TextField(nameSpace , username, 30, textBoxStyle);
 		}
 	}
 
 	IEnumerator WaitToSwitch()
 	{
-		if(string.Compare(username, "Enter Name for High-Scores") != 0){
+		if(string.Compare(username, "Submit Score") != 0)
+		{
 			this.gameObject.GetComponent<Scores>().addUser(username, player.GetComponent<StopScripts>().getScore());
 		}
 		yield return new WaitForSeconds(.2f);
